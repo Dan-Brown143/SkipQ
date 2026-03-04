f9a64a79aaf38f91f9717caba928dd6a6f54c042
Dan <danbrown32323@gmail.com>
2026-03-04 15:59:13 +0000

    update(config): updated the firebase database config for the new database

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAB9aNmaY9r-978WblrfmnY6gGVJR8IRrk",
-  authDomain: "skipq-7ef64.firebaseapp.com",
-  projectId: "skipq-7ef64",
-  storageBucket: "skipq-7ef64.firebasestorage.app",
-  messagingSenderId: "756243761893",
-  appId: "1:756243761893:web:d494ef5013233eccd7e20c"
+  apiKey: "AIzaSyBq6GdINtwloen5NtNdkylH_Bc0Yhz2lBM",
+  authDomain: "skipq-b3a49.firebaseapp.com",
+  projectId: "skipq-b3a49",
+  storageBucket: "skipq-b3a49.firebasestorage.app",
+  messagingSenderId: "660807174169",
+  appId: "1:660807174169:web:97328b8133e421a16e4288"
 };
 
 const app = initializeApp(firebaseConfig);
